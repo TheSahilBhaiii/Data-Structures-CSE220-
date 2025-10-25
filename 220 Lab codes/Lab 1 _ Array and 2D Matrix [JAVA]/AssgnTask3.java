876812fba179574327b72302a98dced6cde4dcf3
Sahil <suhailashrafsahil2003@gmail.com>
2025-10-26 02:56:46 +0600

    Solved lab 1

--- 220 Lab codes/Lab 1 _ Array and 2D Matrix [JAVA]/AssgnTask3.java
+++ 220 Lab codes/Lab 1 _ Array and 2D Matrix [JAVA]/AssgnTask3.java
@@ -5,11 +5,39 @@ class AssgnTask3{
     //YOU ONLY HAVE TO SUBMIT THIS METHOD, NO OTHER DRIVER CODE
     public static void playGame( Integer[][] arena ){
 
-        //For this task you don't need to create any new 2D array
-        //just print the result inside the function
-        
-        //TO DO
+        int row=arena.length;
+        int col=arena[0].length;
+        int counter=0;
+        int points=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(arena[i][j]%50==0&&arena[i][j]!=0){
+                        if(i>0&&arena[i-1][j]==2){
+                            counter++;
+                        }
+                        if(j>0&&arena[i][j-1]==2){
+                            counter++;
+                        }
+                        if(j<col-1&&arena[i][j+1]==2){
+                            counter++;
+                        }
+                        if(i<row-1&&arena[i+1][j]==2){
+                            counter++;
+                        }
+                    }    
+                    }
+                }
+                    points=counter*2;
+                    if(points>=10){
+                        System.out.println("Points Gained: "+points+ " Your team has survived the game");
+                    }
+                    else{
+                        System.out.println("Points Gained: "+points+ " Your team is out");
+                    
+                    }
     }
+            
+        
 
     //DO NOT CHANGE ANY DRIVER CODE BELOW THIS LINE
     public static void main(String[] args){
